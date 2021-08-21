ea1162e557da8cd30ba5c5ff187ea4c5a99c7025
MostafizurSawon <MostafizurSawon@users.noreply.github.com>
2021-08-21 23:30:13 +0600

    ook

--- js/main.js
+++ js/main.js
@@ -1,152 +1,55 @@
-document.getElementById('eight-gb-ram').addEventListener('click', function(){
-    // console.log('clicked');
+// price update
+function updateComponentPrice(componentPriceID, amountValue) {
+    const componentPrice = document.getElementById(componentPriceID);
+    componentPrice.innerText = amountValue;
+    calculateTotal();
+};
 
-   const updateMemory = 0;
+// TOTAL PRICE
+function calculateTotal() {
+    const bestPrice = parseFloat(document.getElementById("best-price").innerText);
+    const ramPrice = parseFloat(document.getElementById("ram-price").innerText);
+    const ssdPrice = parseFloat(document.getElementById("ssd-price").innerText);
+    const deliveryCost = parseFloat(document.getElementById("delivery-cost").innerText);
+    const total = bestPrice + ramPrice + ssdPrice + deliveryCost;
 
-    const updateText = document.getElementById('update-ram');
-    // const updateInt = parseInt(updateText.innerText);
-    updateText.innerText = updateMemory;
-    const totalText = document.getElementById('raw-price');
-    const totalRaw = parseInt(totalText.innerText);
-    const grandTotal = document.getElementById('total');
-    grandTotal.innerText = totalRaw + updateMemory;
-    
-    
-    const bonusRaw = document.getElementById('bonus-total');
-    bonusRaw.innerText = grandTotal.innerText;
+    const totalPrice = document.getElementById("total-price");
+    const totalPriceWithDiscount = document.getElementById("grand-total");
+    totalPrice.innerText = total;
+    totalPriceWithDiscount.innerText = total;
+};
+
+// BUTTON FUNCTION
+function addButtonListeners(btnID, outputId, value) {
+    document.getElementById(btnID).addEventListener("click", function () {
+        updateComponentPrice(outputId, value);
+    });
+};
+
+
+// RAM BTN HANDLER
+addButtonListeners("normal-ram-btn", "ram-price", 0);
+addButtonListeners("extra-ram-btn", "ram-price", 180);
+
+// SSD BTN HANDLER
+addButtonListeners("basic-ssd", "ssd-price", 0);
+addButtonListeners("mid-ssd", "ssd-price", 100);
+addButtonListeners("premium-ssd", "ssd-price", 180);
+
+// DELIVERY COST BUTTON
+addButtonListeners("free-delivery-btn", "delivery-cost", 0);
+addButtonListeners("fast-delivery-btn", "delivery-cost", 20);
+
+// CUPPON CODE HANDLER 
+document.getElementById("promo-code-apply-btn").addEventListener("click", function () {
+    const totalPrice = parseFloat(document.getElementById("total-price").innerText);
+    const promoCode = document.getElementById("promo-code-input");
+
+    if (promoCode.value == "stevekaku") {
+        const total = totalPrice * .8;
+        const totalPriceWithDiscount = document.getElementById("grand-total");
+        totalPriceWithDiscount.innerText = total;
+    };
+
+    promoCode.value = '';
 });
-
-document.getElementById('sixteen-gb-ram').addEventListener('click', function(){
-    // console.log('clicked');
-
-   const updateMemory = 180;
-
-    const updateText = document.getElementById('update-ram');
-    // const updateInt = parseInt(updateText.innerText);
-    updateText.innerText = updateMemory;
-
-
-    // const totalText = document.getElementById('raw-price');
-    // const totalRaw = parseInt(totalText.innerText);
-    const grandTotal = document.getElementById('total');
-    const totalRaw = parseInt(grandTotal.innerText)
-    grandTotal.innerText = totalRaw + updateMemory;
-
-    const bonusRaw = document.getElementById('bonus-total');
-    bonusRaw.innerText = grandTotal.innerText;
-});
-
-document.getElementById('two-fifty-six').addEventListener('click', function(){
-    // console.log('clicked');
-
-   const updateSsd = 0;
-
-    const updateText = document.getElementById('update-storage');
-    // const updateInt = parseInt(updateText.innerText);
-    updateText.innerText = updateSsd;
-
-
-    // const totalText = document.getElementById('raw-price');
-    // const totalRaw = parseInt(totalText.innerText);
-    // const grandTotal = document.getElementById('total');
-    // grandTotal.innerText = totalRaw + updateMemory;
-    const grandTotal = document.getElementById('total');
-    const totalRaw = parseInt(grandTotal.innerText)
-    grandTotal.innerText = totalRaw + updateSsd;
-
-    const bonusRaw = document.getElementById('bonus-total');
-    bonusRaw.innerText = grandTotal.innerText;
-});
-
-document.getElementById('five-twelve').addEventListener('click', function(){
-    // console.log('clicked');
-
-   const updateSsd = 100;
-
-    const updateText = document.getElementById('update-storage');
-    // const updateInt = parseInt(updateText.innerText);
-    updateText.innerText = updateSsd;
-
-
-    // const totalText = document.getElementById('raw-price');
-    // const totalRaw = parseInt(totalText.innerText);
-    // const grandTotal = document.getElementById('total');
-    // grandTotal.innerText = totalRaw + updateSsd;
-    const grandTotal = document.getElementById('total');
-    const totalRaw = parseInt(grandTotal.innerText)
-    grandTotal.innerText = totalRaw + updateSsd;
-
-    const bonusRaw = document.getElementById('bonus-total');
-    bonusRaw.innerText = grandTotal.innerText;
-});
-
-document.getElementById('one-tera').addEventListener('click', function(){
-    // console.log('clicked');
-
-   const updateSsd = 180;
-
-    const updateText = document.getElementById('update-storage');
-    // const updateInt = parseInt(updateText.innerText);
-    updateText.innerText = updateSsd;
-
-
-    // const totalText = document.getElementById('raw-price');
-    // const totalRaw = parseInt(totalText.innerText);
-    // const grandTotal = document.getElementById('total');
-    // grandTotal.innerText = totalRaw + updateSsd;
-    const grandTotal = document.getElementById('total');
-    const totalRaw = parseInt(grandTotal.innerText)
-    grandTotal.innerText = totalRaw + updateSsd;
-
-    const bonusRaw = document.getElementById('bonus-total');
-    bonusRaw.innerText = grandTotal.innerText;
-});
-
-document.getElementById('free-delivery').addEventListener('click', function(){
-    // console.log('clicked');
-
-   const updateDelivery = 0;
-
-    const updateText = document.getElementById('update-delivery');
-    updateText.innerText = updateDelivery;
-    
-    // const totalText = document.getElementById('raw-price');
-    // const totalRaw = parseInt(totalText.innerText);
-    // const grandTotal = document.getElementById('total');
-    // grandTotal.innerText = totalRaw + updateDelivery;
-    const grandTotal = document.getElementById('total');
-    const totalRaw = parseInt(grandTotal.innerText)
-    grandTotal.innerText = totalRaw + updateDelivery;
-
-    const bonusRaw = document.getElementById('bonus-total');
-    bonusRaw.innerText = grandTotal.innerText;
-});
-
-document.getElementById('premium-delivery').addEventListener('click', function(){
-    // console.log('clicked');
-
-   const updateDelivery = 20;
-
-    const updateText = document.getElementById('update-delivery');
-    updateText.innerText = updateDelivery;
-
-    const totalText = document.getElementById('total');
-    const totalRaw = parseInt(totalText.innerText);
-    const grandTotal = totalRaw + 20;
-    totalText.innerText = grandTotal;
-
-    const bonusRaw = document.getElementById('bonus-total');
-    bonusRaw.innerText = totalText.innerText;
-});
-
-document.getElementById('cuopon').addEventListener('click', function(){
-    const cuoponField = document.getElementById('inputCuopon');
-   const userName = cuoponField.value;
-   if (userName == 'stevekaku'){
-    const bonusRaw = document.getElementById('bonus-total');
-    const beforeCuopon =parseInt(bonusRaw.innerText); 
-    const finalAmount = (beforeCuopon * .8);
-    bonusRaw.innerText = finalAmount;
-}
-cuoponField.value = '';
-})
